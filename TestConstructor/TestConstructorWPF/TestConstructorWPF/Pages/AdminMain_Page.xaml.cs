e12bcb1e6c3f8f49d2400a785863de135be61f3e
vZidv <94315999+vZidv@users.noreply.github.com>
2022-04-18 02:15:00 +0300

    ver 1.2.11.5

--- TestConstructor/TestConstructorWPF/TestConstructorWPF/Pages/AdminMain_Page.xaml.cs
+++ TestConstructor/TestConstructorWPF/TestConstructorWPF/Pages/AdminMain_Page.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TestConstructorWPF.Pages
 {
@@ -39,6 +41,39 @@ namespace TestConstructorWPF.Pages
             Authorization authorization = new Authorization();
             authorization.Show();
             admin.Close();
+        }
+
+        private void refresh_button_Click(object sender, RoutedEventArgs e)
+        {
+            connectClass.LoadTable("Select Login,Password,Type From UserTable", dataGridView_Users);
+        }
+
+        private void deletePerson_button_Click(object sender, RoutedEventArgs e)
+        {
+            int r = dataGridView_Users.SelectedIndex;
+
+            string login = null;
+            string password = null;
+
+            for (int i = 0; i < 2;)
+            {
+                switch (i)
+                {
+                    case 0:
+                        TextBlock itemL = dataGridView_Users.Columns[i].GetCellContent(dataGridView_Users.Items[r]) as TextBlock;
+                        login = itemL.Text;
+                        break;
+                    case 1:
+                        TextBlock itemP = dataGridView_Users.Columns[i].GetCellContent(dataGridView_Users.Items[r]) as TextBlock;
+                        password = itemP.Text;
+                        break;
+                }
+                i++;
+            }
+            MySqlConnectClass.SqlConnect();
+            SqlCommand command = new SqlCommand($"DELETE FROM UserTable WHERE Login = N'{login}' and Password ='{password}'", MySqlConnectClass.sqlCon);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Пользователь удалён!");
         }
     }
 }
